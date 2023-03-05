5626e77888215d7993fe4cc3180f54997a927d34
nham ngo <nhamngoinfo@gmail.com>
2023-03-05 20:38:02 +0700

    Initial commit

---
+++ RecyclerViewAndViewHolder/app/src/main/java/com/example/recyclerviewandviewholder/Person.java
@@ -0,0 +1,28 @@
+package com.example.recyclerviewandviewholder;
+
+public class Person {
+    private String name;
+    private boolean isMale;
+
+    public Person(String name, boolean isMale) {
+        this.name = name;
+        this.isMale = isMale;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+
+}
